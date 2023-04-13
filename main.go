6b7af4e25639b0be97c489fb1284622c2fdee72c
Darlene Costa <darl_mel@hotmail.com>
2023-04-13 06:05:45 -0300

    init rest with go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DarleneCosta/go-rest-api/models"
+	"github.com/DarleneCosta/go-rest-api/routes"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "nome 1", Historia: "historia 1"},
+		{Id: 2, Nome: "nome 2", Historia: "historia 2"},
+	}
+	fmt.Println("Iniciando o servidor Rest com Go")
+	routes.HandleRequest()
+}
