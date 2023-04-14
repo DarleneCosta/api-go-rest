36ca1d1770448e75b853c1878148ac361a97c523
Darlene Costa <darl_mel@hotmail.com>
2023-04-14 06:33:52 -0300

    finished crud

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/DarleneCosta/go-rest-api/database"
 	"github.com/DarleneCosta/go-rest-api/models"
 	"github.com/DarleneCosta/go-rest-api/routes"
 )
@@ -12,6 +13,7 @@ func main() {
 		{Id: 1, Nome: "nome 1", Historia: "historia 1"},
 		{Id: 2, Nome: "nome 2", Historia: "historia 2"},
 	}
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
